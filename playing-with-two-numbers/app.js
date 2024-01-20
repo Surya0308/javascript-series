b15851acaa139c52307dd7a296bcb1fdd200a1b6
Your Name <you@example.com>
2024-01-20 09:55:22 +0530

    js

---
+++ playing-with-two-numbers/app.js
@@ -0,0 +1,29 @@
+function addNum(a, b){
+    return a+b;  
+}
+let addTwoNum = addNum(11, 3);
+console.log(addTwoNum);
+
+function subract(x, y){
+    return x-y;
+}
+var result = subract(100, 67);
+console.log(result);
+
+function multiply(x, y){
+    return x*y;
+}
+var result = multiply(100, 67);
+console.log(result);
+
+function divide(x, y){
+    return x/y;
+}
+var result = divide(100, 3);
+console.log(result);
+
+function modulus(x, y){
+    return x%y;
+}
+var result = modulus(100, 33);
+console.log(result);
