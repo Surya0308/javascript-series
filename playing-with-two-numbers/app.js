cdd9cf6441e2ed874d8e3348cd3b317a7acc2470
Your Name <you@example.com>
2024-01-20 23:20:02 +0530

    js

--- playing-with-two-numbers/app.js
+++ playing-with-two-numbers/app.js
@@ -4,11 +4,24 @@ function addNum(a, b){
 let addTwoNum = addNum(11, 3);
 console.log(addTwoNum);
 
+// //another type 
+// function addNum(a, b){
+//     var result = a+b;
+//     console.log(result);
+// }
+// addNum(11, 3);
+
 function subract(x, y){
     return x-y;
 }
 var result = subract(100, 67);
 console.log(result);
+
+// function subract(x, y){
+//     var result = x-y;
+//     console.log(result);
+// }
+// subract(100, 67);
 
 function multiply(x, y){
     return x*y;
