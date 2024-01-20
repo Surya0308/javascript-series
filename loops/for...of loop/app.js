40c1f14ab55d3bbe0185135a5729f8ba47afe0b1
Your Name <you@example.com>
2024-01-21 00:13:14 +0530

    js

--- loops/for...of loop/app.js
+++ loops/for...of loop/app.js
@@ -1,9 +1,22 @@
-const colors = ["red", "green", "blue"];
-for(let color of colors){
-    console.log(color);
-}
+// const colors = ["red", "green", "blue"];
+// for(let color of colors){
+//     console.log(color);
+// }
 
 //output
 //red
 //green
 //blue
+
+const car = {brand: "Toyota", model: "Camry", year: 2020};
+
+let carInfo = "";
+for (let key in car) {
+  carInfo += `${key}: ${car[key]}\n`;
+}
+console.log(carInfo);
+
+//output
+// brand: Toyota
+// model: Camry
+// year: 2020
